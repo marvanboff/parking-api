53fa3965e8dff5af5fd2be15b6ef0f4e6684a557
Marvan Boff <marvan.boff@cwi.com.br>
2017-09-25 19:34:29 -0300

    estrutura projeto

---
+++ src/model/Owner.js
@@ -0,0 +1,19 @@
+// @flow
+class Owner {
+
+    usuario: string;
+    nome: string;
+    celular: string;
+    ramal: number;
+    andar: number;
+    posicao: string;
+    gravatar: string;
+    dataCriacao: Date;
+    dataLogin: Date;
+    participaSorteio: boolean;
+    dataContemplacao: Date;
+    vagaGerencia: boolean;
+    numeroVaga: string;
+    
+
+}
